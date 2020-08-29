2fb7c33a0b62ffced8435cd3fab534f83764d545
Muhammad Nadzmi <nadzmiidzham@gmail.com>
2020-08-30 01:44:44 +0800

    - added checking for active & inactive mode in homepage - refactored SetTimerWidget

--- lib/ui/views/home.page.dart
+++ lib/ui/views/home.page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:pomodoro_timer/ui/viewmodels/timer.viewmodel.dart';
 import 'package:pomodoro_timer/ui/widgets/set-timer.widget.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -9,11 +11,26 @@ class HomePage extends StatelessWidget {
         title: Text('Home Page'),
       ),
       body: Container(
-        child: SetTimerWidget(
-          minValue: 25,
-          maxValue: 50,
-          playTimerCallback: (focusValue, restValue) {
-            print('Focus Value: $focusValue, Rest Value: $restValue');
+        child: Consumer<TimerViewModel>(
+          builder: (context, provider, child) {
+            Widget timer = SizedBox.shrink();
+
+            switch(provider.mode) {
+              case TimerMode.ACTIVE:
+                break;
+              case TimerMode.INACTIVE:
+                timer = SetTimerWidget(
+                  focusTime: provider.focusTime,
+                  restTime: provider.restTime,
+                  playTimerCallback: (focusValue, restValue) {
+                    provider.saveTimerSetting(focusValue, restValue);
+                    print('Focus Value: ${provider.focusTime}, Rest Value: ${provider.restTime}');
+                  },
+                );
+                break;
+            }
+
+            return timer;
           },
         ),
       ),
--- lib/ui/widgets/set-timer.widget.dart
+++ lib/ui/widgets/set-timer.widget.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 class SetTimerWidget extends StatefulWidget {
-  final double minValue, maxValue;
+  final int focusTime, restTime;
   final Function(int, int) playTimerCallback;
 
   SetTimerWidget({
-    this.minValue,
-    @required this.maxValue,
+    this.focusTime,
+    this.restTime,
     @required this.playTimerCallback
   });
 
@@ -16,16 +16,16 @@ class SetTimerWidget extends StatefulWidget {
 
 class _SetTimerWidgetState extends State<SetTimerWidget> {
   bool _isFocus = true;
-  int _division;
-  int _focusValue = 0, _restValue = 0;
-  double _currentValue = 0;
+  double _currentValue;
+  int _focusValue, _restValue;
 
   @override
   void initState() {
     super.initState();
 
-    _division = (widget.maxValue / 5).round();
-    _currentValue = widget.minValue ?? 0;
+    _currentValue = 5;
+    _focusValue = widget.focusTime ?? 5;
+    _restValue = widget.restTime ?? 5;
   }
 
   @override
@@ -35,11 +35,11 @@ class _SetTimerWidgetState extends State<SetTimerWidget> {
         Text(_isFocus? 'FOCUS' : 'REST'),
 
         Slider(
-          divisions: _division,
+          divisions: 9,
           value: _currentValue,
-          min: 0,
-          max: widget.maxValue,
-          label: _currentValue.round().toString(),
+          min: 5,
+          max: 50,
+          label: '${_currentValue.round().toString()} min',
           onChanged: (double value) {
             setState(() {
               _currentValue = value;
