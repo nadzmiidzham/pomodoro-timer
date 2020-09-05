242554286837e66ffb339b582a23a81d9c1c6e23
Muhammad Nadzmi <nadzmiidzham@gmail.com>
2020-09-06 01:46:32 +0800

    refactored GithubApiService: - directly use http library - added function to check for internet connection - decouple service from model & viewmodel by removing initialization of GithubProfileModel as default value (return null instead)

--- lib/core/services/github-api.service.dart
+++ lib/core/services/github-api.service.dart
@@ -1,7 +1,8 @@
 import 'dart:convert';
 
+import 'package:connectivity/connectivity.dart';
+import 'package:http/http.dart';
 import 'package:pomodoro_timer/core/constants/github-api.constant.dart';
-import 'package:http/http.dart' as http;
 import 'package:pomodoro_timer/core/models/github-profile.model.dart';
 import 'package:pomodoro_timer/core/services/cache.service.dart';
 import 'package:pomodoro_timer/locator.dart';
@@ -10,9 +11,6 @@ class GithubApiService {
   CacheService _cacheService = locator<CacheService>();
 
   Future<GithubProfileModel> getGithubProfile() async {
-    http.Response response = await http.get(
-        '${GithubApiConstant.API_URL}${GithubApiConstant.USER_ENDPOINT}/${GithubApiConstant.USERNAME}');
-
     // get data from cache if possible
     String cachedGithubProfile =
         await _cacheService.getCache(GithubApiConstant.API_CACHE_KEY);
@@ -20,13 +18,18 @@ class GithubApiService {
       return GithubProfileModel.fromJson(jsonDecode(cachedGithubProfile));
     }
 
+    // check internet connection
+    var connection = await (Connectivity().checkConnectivity());
+    if (connection == ConnectivityResult.none) {
+      return null;
+    }
+
+    Response response = await get(
+        '${GithubApiConstant.API_URL}${GithubApiConstant.USER_ENDPOINT}/${GithubApiConstant.USERNAME}');
+
     // if failed response, show default data
     if (response.statusCode != 200) {
-      return GithubProfileModel(
-        name: GithubApiConstant.GITHUB_PROFILE_NAME,
-        profileLink: GithubApiConstant.GITHUB_PROFILE_URL,
-        profilePictureLink: GithubApiConstant.GITHUB_PROFILE_IMAGE_URL,
-      );
+      return null;
     }
 
     // if request is first time, cache response body
