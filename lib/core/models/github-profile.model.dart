7997d2651cd92949b4abb49900fd098b698a3973
Muhammad Nadzmi <nadzmiidzham@gmail.com>
2020-09-06 01:44:32 +0800

    added email attribute in GithubApiModel

--- lib/core/models/github-profile.model.dart
+++ lib/core/models/github-profile.model.dart
@@ -1,18 +1,21 @@
 class GithubProfileModel {
   String profileLink;
   String name;
+  String email;
   String profilePictureLink;
 
   GithubProfileModel({
     this.profileLink,
     this.name,
     this.profilePictureLink,
+    this.email,
   });
 
   Map toJson() => {
         'profileLink': this.profileLink,
         'name': this.name,
         'profilePictureLink': this.profilePictureLink,
+        'email': this.email,
       };
 
   factory GithubProfileModel.fromJson(Map<String, dynamic> json) {
@@ -20,10 +23,11 @@ class GithubProfileModel {
       profileLink: json['html_url'],
       name: json['name'],
       profilePictureLink: json['avatar_url'],
+      email: json['email'],
     );
   }
 
   String toString() {
-    return '{profileLink: ${this.profileLink}, name: ${this.name}, profilePictureLink: ${this.profilePictureLink}}';
+    return '{profileLink: ${this.profileLink}, name: ${this.name}, profilePictureLink: ${this.profilePictureLink}, email: ${this.email}}';
   }
 }
