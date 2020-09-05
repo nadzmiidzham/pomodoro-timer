9ccdca6e19c6c075fa918a86a35eb222b4d3fc8e
Muhammad Nadzmi <nadzmiidzham@gmail.com>
2020-09-06 01:48:50 +0800

    - setstate for openWebBrowser() - added openEmailApp() to open email app - initialize empty GithubProfileModel if cannot get from cache or API - for each empty GithubProfileModel attributes, added condition to show initial value from constant

--- lib/ui/viewmodels/about.viewmodel.dart
+++ lib/ui/viewmodels/about.viewmodel.dart
@@ -1,3 +1,4 @@
+import 'package:pomodoro_timer/core/constants/github-api.constant.dart';
 import 'package:pomodoro_timer/core/models/github-profile.model.dart';
 import 'package:pomodoro_timer/core/services/github-api.service.dart';
 import 'package:pomodoro_timer/locator.dart';
@@ -6,35 +7,57 @@ import 'package:url_launcher/url_launcher.dart';
 
 class AboutViewModel extends BaseViewModel {
   GithubApiService _githubApiService = locator<GithubApiService>();
-  GithubProfileModel _profile =
-      GithubProfileModel(name: '', profileLink: null, profilePictureLink: null);
+  GithubProfileModel _profile;
 
   // getter
   String get profileLink {
-    return _profile.profileLink;
+    return _profile.profileLink ?? GithubApiConstant.GITHUB_PROFILE_URL;
   }
 
   String get name {
-    return _profile.name;
+    return _profile.name ?? GithubApiConstant.GITHUB_PROFILE_NAME;
   }
 
   String get profilePictureLink {
     return _profile.profilePictureLink;
   }
 
+  String get email {
+    return _profile.email ?? GithubApiConstant.GITHUB_PROFILE_EMAIL;
+  }
+
   // init methods
   initProfile() async {
     setState(ViewState.BUSY);
-    _profile = await _githubApiService.getGithubProfile();
+    _profile =
+        await _githubApiService.getGithubProfile() ?? GithubProfileModel();
     setState(ViewState.IDLE);
   }
 
   // action methods
   openWebBrowser(String url) async {
+    setState(ViewState.BUSY);
+
     if (await canLaunch(url)) {
       launch(url);
     } else {
       throw 'Cannot launch web browser.';
     }
+
+    setState(ViewState.IDLE);
+  }
+
+  openEmailApp(String email) async {
+    setState(ViewState.BUSY);
+
+    String emailUrl = 'mailto:$email';
+
+    if (await canLaunch(emailUrl)) {
+      launch(emailUrl);
+    } else {
+      throw 'Cannot launch email app.';
+    }
+
+    setState(ViewState.IDLE);
   }
 }
--- lib/ui/views/about.view.dart
+++ lib/ui/views/about.view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pomodoro_timer/ui/shared/asset.constant.dart';
 import 'package:pomodoro_timer/ui/viewmodels/about.viewmodel.dart';
 import 'package:pomodoro_timer/ui/viewmodels/base.viewmodel.dart';
 import 'package:pomodoro_timer/ui/views/base.view.dart';
@@ -19,13 +20,16 @@ class AboutView extends StatelessWidget {
               ? CircularProgressIndicator()
               : Column(
                   children: [
-                    InkWell(
-                      child: _profilePicture(viewModel.profilePictureLink),
-                      onTap: () {
-                        viewModel.openWebBrowser(viewModel.profileLink);
-                      },
-                    ),
+                    viewModel.profilePictureLink == null
+                        ? _profilePicture(AssetConstant.PROFILE_IMAGE, true)
+                        : _profilePicture(viewModel.profilePictureLink, false,
+                            onTap: () {
+                            viewModel.openWebBrowser(viewModel.profileLink);
+                          }),
                     _name(viewModel.name),
+                    _email(viewModel.email, onTap: () {
+                      viewModel.openEmailApp(viewModel.email);
+                    }),
                   ],
                 ),
         ),
@@ -33,15 +37,27 @@ class AboutView extends StatelessWidget {
     );
   }
 
-  Widget _profilePicture(String url) {
-    return Image.network(
-      url,
-      loadingBuilder: (context, child, loadingProgress) =>
-          loadingProgress == null ? child : CircularProgressIndicator(),
+  Widget _profilePicture(String path, bool asset, {VoidCallback onTap}) {
+    return InkWell(
+      child: asset
+          ? Image.asset(path)
+          : Image.network(
+              path,
+              loadingBuilder: (context, child, loadingProgress) =>
+                  loadingProgress == null ? child : CircularProgressIndicator(),
+            ),
+      onTap: onTap,
     );
   }
 
   Widget _name(String name) {
     return Text(name);
+  }
+
+  Widget _email(String email, {VoidCallback onTap}) {
+    return InkWell(
+      child: Text(email),
+      onTap: onTap,
+    );
   }
 }
