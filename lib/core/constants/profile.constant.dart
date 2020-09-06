2e534141336f9acd13f917e9bd4a978a31254926
Muhammad Nadzmi <nadzmiidzham@gmail.com>
2020-09-06 11:25:18 +0800

    added ProfileConstant

---
+++ lib/core/constants/profile.constant.dart
@@ -0,0 +1,13 @@
+class ProfileConstant {
+  static const List<Map<String, String>> SOCIAL_MEDIA_LINK_LIST = [
+    {
+      'iconPath': 'assets/images/github_logo.png',
+      'profileLink': 'https://github.com/nadzmiidzham'
+    },
+    {
+      'iconPath': 'assets/images/linkedin_logo.png',
+      'profileLink':
+          'https://www.linkedin.com/in/muhammad-nadzmi-mohamed-idzham-a5b35863/'
+    }
+  ];
+}
